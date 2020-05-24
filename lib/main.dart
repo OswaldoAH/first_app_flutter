8f58d6185803600521402dd03d07b6c2c3b21a91
Oswaldo Alvarez <mynor4755@hotmail.com>
2020-05-24 15:01:43 -0600

    Modificando Pantalla principal para iterar una lista y crear x cantidad de answer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,37 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.indigo,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'First app with Flutter'),
     );
   }
-} 
+}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,50 +31,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _questionIndex=0;
-  var _questions=[
-    'Cuál es tu color favorito?',
-    'Cuál es tu animal favorito?  ',
-    ];
+  int _questionIndex = 0;
+  var _questions = [
+    {
+      'Pregunta': 'Cuál es tu color favorito?',
+      'Respuestas': ['Rojo','Negro','Blanco','Azul']
+    },
+    {
+      'Pregunta': 'Cuál es tu Animal favorito?',
+      'Respuestas': ['Conejo','Gato','Perro','Loro']
+    },
+    {
+      'Pregunta': 'Cuál es tu lenguaje de programación favorito?',
+      'Respuestas': ['Java','Ruby','Python','Dart']
+    }
+  ];
   void _incrementIndex() {
     setState(() {
-      if (_questionIndex != _questions.length-1){
+      if (_questionIndex != _questions.length - 1) {
         _questionIndex++;
       }
     });
   }
-  void _decrementIndex(){
-    setState(() {
-      if (_questionIndex != 0) {
-        _questionIndex--;
-      }
-    });
-  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-
       ),
       body: Column(
         children: <Widget>[
           Question(
-          _questions[_questionIndex],
+            _questions[_questionIndex]['Pregunta'],
           ),
-          Row(
-            children: <Widget>[
-              RaisedButton(
-                child: Text("Anterior"),
-                onPressed: _decrementIndex,
-              ),
-              RaisedButton(
-                child: Text("Siguiente"),
-                onPressed: _incrementIndex,
-              ),
-            ],
-          )
+          ...(_questions[_questionIndex]['Respuestas'] as List<String>)
+          .map((respuesta) {
+            return Answer(
+              answerText: respuesta,
+              colorButton: Colors.black,
+              colorText: Colors.white,
+              selectHandler: _incrementIndex,
+            );
+          }).toList()
+          //Answer('Siguiente',Colors.white),
         ],
       ),
     );
